1a8c7186df2532a40be429c283191b51b87f1882
Mikael De Jesus <mikaeldejesus444@gmail.com>
2024-05-18 11:43:32 -0300

    att

--- alura projetos/aluramidi-curso-arquivos-iniciais/aluramidi-curso-arquivos-iniciais/script.js
+++ alura projetos/aluramidi-curso-arquivos-iniciais/aluramidi-curso-arquivos-iniciais/script.js
@@ -4,36 +4,35 @@ acessar o atributo de cada elemento e adicionar uma função para que saia som
 
 
 //função que acessa os atributos dos elementos da lista e executa uma  função em cima deles
-function tocarSom(idElementAudio){
-   const elemento = document.querySelector(idElementAudio);
-    
-   if(elemento === null){
-    console.log("Elemento não encontrado")
-   }
-   if(elemento != null && elemento.localName === "audio"){
-     elemento.play();
-   }
+function tocarSom(idElementAudio) {
+    const elemento = document.querySelector(idElementAudio);
+
+    if (elemento && elemento.localName === "audio") {
+        elemento.play();
+    }else{
+        console.log("Elemento não encontrado")
+    }
 }
 
-//referência que guarda todos os elementos das teclas em uma lista
+    //referência que guarda todos os elementos das teclas em uma lista
     const listaDeTeclas = document.querySelectorAll('.tecla')
-//looping para para adicionar a função 'tocarSom' em cada um dos elementos da listaDeTeclas
- for(let contador = 0; contador < listaDeTeclas.length; contador++){
-    const tecla = listaDeTeclas[contador]
-//referência para acessar a class do elemento[tecla], para poder  usar a função 'tocarSom' nela
-    const instrumento = tecla.classList[1]
-//referência usada para concatenar a class do tecla com o nome  do instrumento,para criar o id do elemento audio correspondente
-    
-const nomeDaNota = `#som_${instrumento}`
-    tecla.onclick = ()=>tocarSom(nomeDaNota);
+    //looping para para adicionar a função 'tocarSom' em cada um dos elementos da listaDeTeclas
+    for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+        const tecla = listaDeTeclas[contador]
+        //referência para acessar a class do elemento[tecla], para poder  usar a função 'tocarSom' nela
+        const instrumento = tecla.classList[1]
+        //referência usada para concatenar a class do tecla com o nome  do instrumento,para criar o id do elemento audio correspondente
 
-    tecla.onkeydown = function(evento){
-        if(evento.key == 'space' ||  evento.key == 'Enter'){
-        tecla.classList.add('ativa')
-    } 
-    tecla.onkeyup = ()=> tecla.classList.remove('ativa');
-   }
- }
+        const nomeDaNota = `#som_${instrumento}`
+        tecla.onclick = () => tocarSom(nomeDaNota);
+
+        tecla.onkeydown = function (evento) {
+            if (evento.key === 'space' || evento.key === 'enter') {
+                tecla.classList.add('ativa')
+            }
+            tecla.onkeyup = () => tecla.classList.remove('ativa');
+        }
+    }
 /*function tocarSomPom(){
 document.querySelector('#som_tecla_pom').play();
 }
